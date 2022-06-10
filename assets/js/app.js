3144d5887c7af0cb780d3ed51d461ba8f7f207bd
yang <joey674901205@gmail.com>
2022-06-10 16:06:02 -0400

    add a api key

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
-console.log("123");
+var searchCityBtn = document.querySelector("#btn-search-city");
+
+function searchCity() {
+    console.log("clicked search city button");
+}
+
+searchCityBtn.addEventListener("click", searchCity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+// API key: 4f585493f941e0eeef3857860aa417e6
