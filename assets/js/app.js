6e1808fd7a08f4161e5d062d3dfa0389f6d565c6
yang <joey674901205@gmail.com>
2022-06-10 17:28:56 -0400

    click search button can save info into localstorage and create a list under history

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,48 @@
+var cityData = [];
 var searchCityBtn = document.querySelector("#btn-search-city");
+var searchCity = document.querySelector("#search-city");
+var historyEl = document.querySelector(".history-part");
 
-function searchCity() {
-    console.log("clicked search city button");
+function searchCityEl() {
+    var city = searchCity.value.toLowerCase();
+    if (!city) {
+        alert("please enter a city");
+    } else {
+        cityData.push(city);
+        searchCity.value = "";
+        console.log(cityData);
+
+        searchHistoryEl(city);
+        savedata();
+    }
 }
 
-searchCityBtn.addEventListener("click", searchCity)
+function searchHistoryEl(info) {
+    var cityList = document.createElement("button");
+    cityList.classList.add("city-list", "mt-2");
+    cityList.textContent = info;
+    historyEl.appendChild(cityList);
+    console.log(historyEl);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function savedata() {
+    localStorage.setItem("city", cityData);
+}
+
+searchCityBtn.addEventListener("click", searchCityEl)
 
 
 
