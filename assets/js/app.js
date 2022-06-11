31a69df20ef6af66a4b9230f5a469c556e289404
yang <joey674901205@gmail.com>
2022-06-11 11:05:30 -0400

    can reload search city from localstorage and clear search history

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ var cityData = [];
 var searchCityBtn = document.querySelector("#btn-search-city");
 var searchCity = document.querySelector("#search-city");
 var historyEl = document.querySelector(".history-part");
+var btnClearEl = document.querySelector("#btn-clear");
 
+// starting search
 function searchCityEl() {
     var city = searchCity.value.toLowerCase();
     if (!city) {
@@ -10,42 +12,64 @@ function searchCityEl() {
     } else {
         cityData.push(city);
         searchCity.value = "";
-        console.log(cityData);
 
         searchHistoryEl(city);
+        getApiSearch()
         savedata();
     }
 }
 
+// create search history in historyEl(".history-part")
 function searchHistoryEl(info) {
     var cityList = document.createElement("button");
     cityList.classList.add("city-list", "mt-2");
     cityList.textContent = info;
     historyEl.appendChild(cityList);
-    console.log(historyEl);
 }
 
+// clear search history
+function clearHistoryEl() {
+    var remove = historyEl;
 
+    while (remove.hasChildNodes()) {
+        remove.removeChild(remove.firstChild);
+    }
 
+    cityData.length = 0;
+    // console.log(cityData);
+    window.localStorage.clear();
+}
 
+// refresh page, load localstorage
+function loadsearch() {
+    var load = localStorage.getItem("city");
+    var loadArr = load.split(",");
+    for (var i=0; i<loadArr.length; i++) {
+        searchHistoryEl(loadArr[i]);
+    }
+}
 
-
-
-
-
-
-
-
-
-
+// save search city in localstorage
 function savedata() {
     localStorage.setItem("city", cityData);
 }
 
+// get API
+function getApiSearch() {
+    console.log("123");
+}
+
+
+
+
+
+
+
+
 searchCityBtn.addEventListener("click", searchCityEl)
+btnClearEl.addEventListener("click", clearHistoryEl)
 
-
-
+loadsearch();
 
 
 
@@ -57,4 +81,7 @@ searchCityBtn.addEventListener("click", searchCityEl)
 
 
 // https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
 // API key: 4f585493f941e0eeef3857860aa417e6
